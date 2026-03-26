f58cbf60bc2932d5e6946e02c3a60999f5bef33e
Zhu Lingkai <zhulingkai123@gmail.com>
2026-03-26 13:53:47 +0200

    update run sh

--- run.sh
+++ run.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
-#SBATCH --time=60:00:00
+#SBATCH --time=80:00:00
 #SBATCH --mem=80G
-#SBATCH --cpus-per-gpu=40
-#SBATCH --gpus=2
-#SBATCH --partition=gpu-h200-141g-ellis
+#SBATCH --cpus-per-gpu=20
+#SBATCH --gpus=4
+#SBATCH --partition=gpu-v100-16g
 
 module load scicomp-python-env
-# source /scratch/work/zhul2/code/assignment1-basics/.venv/bin/activate
-# module load triton/2025.1-gcc
-# module load cuda
-# python main.py \
-#   "/scratch/shareddata/dldata/imagenet-1k-wds/imagenet-1k-wds/" \
-#   --arch resnet50 \
-#   --batch-size 256 \
-#   --workers 20 \
-#   --dist-url 'tcp://127.0.0.1:10000'\
-#   --dist-backend 'nccl'\
-#   --world-size 1 \
-#   --rank 0 \
-#   --multiprocessing-distributed \
-#   --compile
 
-TORCHDYNAMO_CAPTURE_SCALAR_OUTPUTS=1 python main_moco.py \
+python main_moco.py \
   "/scratch/shareddata/dldata/imagenet-1k-wds/imagenet-1k-wds/" \
   -a resnet50 \
   --lr 0.03 \
@@ -32,4 +18,5 @@ TORCHDYNAMO_CAPTURE_SCALAR_OUTPUTS=1 python main_moco.py \
   --world-size 1 \
   --rank 0 \
   --dist-backend 'nccl' \
-  --compile
+  --compile \
+  --resume /scratch/work/zhul2/code/moco/saved_model/resnet50/mocov1/checkpoint_0190.pth.tar
